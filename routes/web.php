2c2c9b789813ccd7ad0ab889795e45f84e8b7fd8
Henriq_Fonseca <henrique12095@gmail.com>
2022-09-11 20:04:49 -0300

    Criei uma rota para a home utilizando controllers e views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+        Route::get('/', [HomeController::class, "__invoke"]);
