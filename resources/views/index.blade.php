00367f5218b4224d86164f67bd51bb63c302f17c
Henriq_Fonseca <henrique12095@gmail.com>
2022-09-17 04:05:28 -0300

    adicionei css ao botãão do formulário

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,31 +17,37 @@
                 <input type="text" class="form-control" id="nomelocal" name="nomelocal"required >
                 <small>Nome do local</small>
             </div>
+            <br>
             <div class="form-group">
                 <label for="cep">CEP</label>
                 <input type="text" class="form-control" maxlength="9" id="cep" name="cep" required>
                 <small>00000-000</small>
             </div>
+            <br>
             <div class="form-group">
                 <label for="logradouro">Logradouro</label>
                 <input type="text" class="form-control" id="logradouro"  name="logradouro" required>
                 <small>Rua.</small>
             </div>
+            <br>
             <div class="form-group">
                 <label for="bairro">Bairro</label>
                 <input type="text" class="form-control" id="bairro" name="bairro" required>
                 <small>Bairro</small>
             </div>
+            <br>
             <div class="form-group">
                 <label for="localidade">Localidade</label>
                 <input type="text" class="form-control" id="localidade" name="localidade" required>
                 <small>Cidade</small>
             </div>
+            <br>
             <div class="form-group">
                 <label for="uf">UF</label>
                 <input type="text" class="form-control" id="uf" name="uf" required>
                 <small>Estado</small>
             </div>
+            <br>
             <div class="form-group">
                 <label for="horario">Horário de Funcionamento</label>
                 <input type="text" class="form-control" id="horario" name="horario" >
@@ -53,7 +59,7 @@
                 <input type="text" class="form-control" id="url" name="url" >
                 <small>Endereço do Site</small>
             </div>
-            <button type="submit">Enviar</button>
+            <button class="btn btn-primary" type="submit">Enviar</button>
         </form>
     </div>
 
