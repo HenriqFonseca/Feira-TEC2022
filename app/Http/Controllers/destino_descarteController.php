59eccdfbbe1fc3536a55b04ec50c238340ae379a
Henriq_Fonseca <henrique12095@gmail.com>
2022-09-17 03:31:13 -0300

    adicionei comentáarios no Controller para facilitar entendimento

--- app/Http/Controllers/destino_descarteController.php
+++ app/Http/Controllers/destino_descarteController.php
@@ -8,7 +8,7 @@ use App\Models\LocalDestino;
 class destino_descarteController extends Controller
 {
     public function pegarDados(Request $request){
-        $nomelocal = $request->input('nomelocal');
+        $nomelocal = $request->input('nomelocal'); //$NomeDaVariável = $request->input('o que foi definindo no "name" do formulario HTML')
         $cep = $request->input('cep');
         $logradouro = $request->input('logradouro');
         $bairro = $request->input('bairro');
@@ -17,7 +17,8 @@ class destino_descarteController extends Controller
         $horario = $request->input('horario');
         $url = $request->input('url');
 
-        $isInsertSuccess = LocalDestino::insert(['nomelocal' => $nomelocal,
+
+        $isInsertSuccess = LocalDestino::insert(['nomelocal' => $nomelocal, //sistema de inserts ['NomeDaTabela'=> variavel do que o usuário digitou que foi definida acima ^]
         'cep' => $cep,
         'logradouro' => $logradouro,
         'bairro' => $bairro,
@@ -27,6 +28,7 @@ class destino_descarteController extends Controller
         'site_url' => $url,
     
     ]);
+    //para verificar se o insert deu certo
     if($isInsertSuccess) echo '<h1>Insert deu certo</h1>';
     else echo '<h1>Insert deu errado</h1>';
     }
