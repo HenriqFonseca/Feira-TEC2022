126aac5eaaa743bea28d70de7dc2b00f22f2d4c7
Henriq_Fonseca <henrique12095@gmail.com>
2022-09-20 16:38:36 -0300

    mudei o sw

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,24 @@
-self.addEventListener('install', event=> {
-
-    console.log('sw ./ => installing...');
-    
-    console.log("sw ./ => install event detected e cat.svg cacheado!!!");
-});
-
-self.addEventListener('activate', event => {
-    console.log('sw ./ => Evento activate ocorreu, agora pronto para interceptar fetches');
-});
-
-self.addEventListener('fetch', event => {
-    console.log("sw ./ => detectei um evento fetch para o recurso abaixo:");
-    console.log("sq ./ =>"+event.request.url);
-
-    const url = new URL (event.request.url);
-
-});
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/main.js'
+  ];
+  
+  /* Start the service worker and cache all of the app's content */
+  self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
+  
+  /* Serve cached content when offline */
+  self.addEventListener('fetch', function(e) {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  });
